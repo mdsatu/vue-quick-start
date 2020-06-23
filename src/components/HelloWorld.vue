825e2e19df31b901b7b4428e1addaa9a30ad6197
ndsatu <mdsatu.pro@gmail.com>
2020-06-24 00:21:11 +0600

    Project ini with AdminLTE

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login-box">
+    <div class="login-logo">
+      <a href="">Welcome to</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form action="" method="post">
+        <div class="form-group has-feedback">
+          <input type="text" name="email" class="form-control" placeholder="Email"  required>
+          <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input name="password" type="password" class="form-control" placeholder="Password" required>
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="row">
+          <div class="col-xs-8">
+          </div>
+          <!-- /.col -->
+          <div class="col-xs-4">
+            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <!-- <a href="#">I forgot my password</a><br> -->
+    </div>
+    <!-- /.login-box-body -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  mounted(){
+    document.body.classList.add("login-page");
+    console.log('Component mounted');
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
